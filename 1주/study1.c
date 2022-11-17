b03f4b82c19ee0751bce03fa2ea9cd65ff8edb61
leekyoungseob <114080582+Glodmilk@users.noreply.github.com>
2022-11-17 19:22:59 +0900

    Create study1.c

---
+++ 1주/study1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char* fname = "in.txt";
+	FILE* f = fopen(fname, "w");
+	fprintf(f,"%d %d %d %d %d %d", 5, 23, 50, 234, 170, 315);
+
+	fclose(f);
+
+	return 0;
+}
